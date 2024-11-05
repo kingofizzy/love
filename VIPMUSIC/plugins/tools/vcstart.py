41c3656d9cc0543a3369e24a7e823825591bc73b
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-11-05 21:42:25 +0530

    Update vcstart.py

--- VIPMUSIC/plugins/tools/vcstart.py
+++ VIPMUSIC/plugins/tools/vcstart.py
@@ -140,9 +140,7 @@ async def stop_group_call(c: Client, m: Message):
                 )
             )
         ):
-            return
-        await assistant.invoke(DiscardGroupCall(call=group_call))
-        await msg.edit_text("🤧💫 𝑆𝑜𝑛𝑔 𝐸𝑣𝑎𝑛𝑑𝑎 𝐸𝑛𝑑 𝑃𝑎𝑛𝑛𝑢𝑛𝑎𝑡ℎ𝑢 🥹🤌🏻")
+          
     except Exception as e:
         if "GROUPCALL_FORBIDDEN" in str(e):
             try:
@@ -183,8 +181,6 @@ async def stop_group_call(c: Client, m: Message):
                         can_promote_members=False,
                     ),
                 )
-                await msg.edit_text("🤧💫 𝑆𝑜𝑛𝑔 𝐸𝑣𝑎𝑛𝑑𝑎 𝐸𝑛𝑑 𝑃𝑎𝑛𝑛𝑢𝑛𝑎𝑡ℎ𝑢 🥹🤌🏻")
-                await VIP.st_stream(chat_id)
-                await set_loop(chat_id, 0)
+              
             except:
                 await msg.edit_text("🍷 𝑫𝒂𝒊 𝒂𝒅𝒎𝒊𝒏 𝒗𝒄 𝒔𝒕𝒂𝒓𝒕 𝒑𝒂𝒏𝒏𝒊𝒕𝒉𝒂 𝒖𝒏𝒏𝒂 𝒎𝒂𝒂𝒓𝒊 𝒏𝒂𝒍𝒂 𝒗𝒂 𝒚𝒂𝒓𝒖 𝒊𝒍𝒍𝒂 𝒅𝒂 😻")
