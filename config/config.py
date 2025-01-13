b104c5f9e6b6a32e9ed6a0e27839e93889e576a8
Hyper Network <123313247+kingofizzy@users.noreply.github.com>
2025-01-13 10:56:20 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -54,7 +54,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/kingofizzy/love",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
